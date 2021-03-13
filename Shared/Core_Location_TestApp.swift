1f0d0210b6e4bb61a24fa47a17b0cfb2c1da91f8
Andy Ibanez <github@andyibanez.com>
2021-03-13 18:59:44 -0400

    Initial Commit

---
+++ Shared/Core_Location_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_Location_TestApp.swift
+//  Shared
+//
+//  Created by Andy Ibanez on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Core_Location_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
