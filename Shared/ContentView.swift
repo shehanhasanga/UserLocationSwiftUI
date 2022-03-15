fc2656325a229fc43de4585becd1f50241bd1814
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-03-15 10:29:30 +0530

    added files

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,139 @@
 //
 
 import SwiftUI
+import CoreLocation
+import MapKit
 
 struct ContentView: View {
+    @StateObject var locationViewModel = LocationViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
+        switch locationViewModel.authorizationStatus {
+        case .notDetermined:
+            AnyView(RequestLocationView())
+                .environmentObject(locationViewModel)
+        case .restricted:
+            ErrorView(errorText: "Location use is restricted.")
+        case .denied:
+            ErrorView(errorText: "The app does not have location permissions. Please enable them in settings.")
+        case .authorizedAlways, .authorizedWhenInUse:
+//            TrackingView()
+//                .environmentObject(locationViewModel)
+          MapView()
+        default:
+            Text("Unexpected status")
+        }
+    }
+}
+
+struct RequestLocationView: View {
+    @EnvironmentObject var locationViewModel: LocationViewModel
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "location.circle")
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Button(action: {
+                locationViewModel.requestPermission()
+            }, label: {
+                Label("Allow tracking", systemImage: "location")
+            })
+            .padding(10)
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            Text("We need your permission to track you.")
+                .foregroundColor(.gray)
+                .font(.caption)
+        }
+    }
+}
+
+struct ErrorView: View {
+    var errorText: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "xmark.octagon")
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+            Text(errorText)
+        }
+        .padding()
+        .foregroundColor(.white)
+        .background(Color.red)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+}
+
+struct MapView:View{
+  @StateObject var locationViewModel = LocationViewModel()
+  @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.785834, longitude: -122.406417), latitudinalMeters: 1, longitudinalMeters: 1)
+  @State var tracking : MapUserTrackingMode = .follow
+  var body: some View {
+//    Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+//      .edgesIgnoringSafeArea(.all)
+    Map(coordinateRegion: $region, showsUserLocation: false, userTrackingMode: .constant(.follow), annotationItems: locationViewModel.pins){
+      pin in
+      MapPin(coordinate: CLLocationCoordinate2D(latitude: pin.location.coordinate.latitude, longitude: pin.location.coordinate.longitude), tint: .red)
+    }
+               .frame(width: 400, height: 300)
+               .edgesIgnoringSafeArea(.all)
+
+  }
+}
+
+struct TrackingView: View {
+    @EnvironmentObject var locationViewModel: LocationViewModel
+    
+    
+    var body: some View {
+        VStack {
+            VStack {
+                PairView(
+                    leftText: "Latitude:",
+                    rightText: String(coordinate?.latitude ?? 0)
+                )
+                PairView(
+                    leftText: "Longitude:",
+                    rightText: String(coordinate?.longitude ?? 0)
+                )
+                PairView(
+                    leftText: "Altitude",
+                    rightText: String(locationViewModel.lastSeenLocation?.altitude ?? 0)
+                )
+                PairView(
+                    leftText: "Speed",
+                    rightText: String(locationViewModel.lastSeenLocation?.speed ?? 0)
+                )
+                PairView(
+                    leftText: "Country",
+                    rightText: locationViewModel.currentPlacemark?.country ?? ""
+                )
+                PairView(leftText: "City", rightText: locationViewModel.currentPlacemark?.administrativeArea ?? ""
+                )
+            }
             .padding()
+        }
+    }
+    
+    var coordinate: CLLocationCoordinate2D? {
+        locationViewModel.lastSeenLocation?.coordinate
+    }
+}
+
+struct PairView: View {
+    let leftText: String
+    let rightText: String
+    
+    var body: some View {
+        HStack {
+            Text(leftText)
+            Spacer()
+            Text(rightText)
+        }
     }
 }
 
---
+++ Shared/LocationViewModel.swift
@@ -0,0 +1,74 @@
+//
+//  LocationViewModel.swift
+//  Core Location Test
+//
+//  Created by Andy Ibanez on 3/13/21.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+/*
+ snippet 1
+class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        locationManager = CLLocationManager()
+        
+        super.init()
+        locationManager.delegate = self
+    }
+}
+ */
+
+class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var authorizationStatus: CLAuthorizationStatus
+    @Published var lastSeenLocation: CLLocation?
+    @Published var currentPlacemark: CLPlacemark?
+  @Published var pins : [MapLocation] = []
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        locationManager = CLLocationManager()
+        authorizationStatus = locationManager.authorizationStatus
+        
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 0.4
+        locationManager.startUpdatingLocation()
+    }
+    
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastSeenLocation = locations.first
+        fetchCountryAndCity(for: locations.first)
+        pins.append(MapLocation(location: locations.last!))
+    }
+    
+    func fetchCountryAndCity(for location: CLLocation?) {
+        guard let location = location else { return }
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            self.currentPlacemark = placemarks?.first
+        }
+    }
+
+  
+}
+
+
+struct MapLocation: Identifiable {
+    let id = UUID()
+    let location:CLLocation
+}
